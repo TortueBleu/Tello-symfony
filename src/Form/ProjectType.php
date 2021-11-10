2fa4be752f45b9a3b12a3da28caf7c708eb15602
Lucass-Dev <liger.lucas33@gmail.com>
2021-11-10 13:42:06 +0100

    ManyToMany for users is now functional

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Project;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class ProjectType extends AbstractType
 {
@@ -14,8 +17,17 @@ class ProjectType extends AbstractType
         $builder
             ->add('name')
             ->add('timestamp')
-            ->add('users')
-        ;
+            ->add('users', EntityType::class, [
+            // looks for choices from this entity
+            'class' => User::class,
+
+            // uses the User.username property as the visible option string
+            'choice_label' => 'name',
+
+            // used to render a select box, check boxes or radios
+            'multiple' => true,
+            'expanded' => true,
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -15,8 +15,17 @@ class UserType extends AbstractType
             ->add('name')
             ->add('mail')
             ->add('timestamp')
-            ->add('projects')
-        ;
+            ->add('users', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Project::class,
+
+                // uses the User.username property as the visible option string
+                'choice_label' => 'name',
+
+                // used to render a select box, check boxes or radios
+                'multiple' => true,
+                'expanded' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
