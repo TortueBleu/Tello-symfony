556639df078996dcd4394ece61238d4f8cf29109
Lucass-Dev <liger.lucas33@gmail.com>
2021-11-10 13:43:42 +0100

    Update UserType.php

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class UserType extends AbstractType
 {
